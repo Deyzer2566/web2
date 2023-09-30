a908018ff7e32a9063658281c6c11e077a68c07d
ScaryCrocodile <50776487+Deyzer2566@users.noreply.github.com>
2023-09-30 10:59:50 +0300

    AreaCheckServlet создает и возвращает свою страницу

--- src/main/java/AreaCheckServlet.java
+++ src/main/java/AreaCheckServlet.java
@@ -1,9 +1,13 @@
+import Point.PointInArea;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 @WebServlet(name="AreaCheckServlet")
 public class AreaCheckServlet extends HttpServlet {
@@ -17,6 +21,39 @@ public class AreaCheckServlet extends HttpServlet {
         } catch (IllegalArgumentException e) {
             request.getSession().setAttribute("exceptionMessage", e.getMessage());
         }
-        getServletContext().getRequestDispatcher("/page.jsp").forward(request, response);
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("<html>" +
+                        "<body>" +
+                            "<table>" +
+                            "<thead>" +
+                            "<tr>" +
+                            "<th>X</th>" +
+                            "<th>Y</th>" +
+                            "<th>R</th>" +
+                            "<th>Попадание</th>" +
+                            "</thead>" +
+                            "<tbody>");
+        if(request.getSession().getAttribute("points")!=null){
+            List<PointInArea> points = (List<PointInArea>) request.getSession().getAttribute("points");
+            for(PointInArea pointInArea: points){
+                out.println("<tr><td>"+Float.valueOf(pointInArea.getX()).toString()+
+                        "</td><td>"+Float.valueOf(pointInArea.getY()).toString()+
+                        "</td><td>"+Float.valueOf(pointInArea.getR()).toString()+
+                        "</td><td>"+Boolean.valueOf(pointInArea.isInArea()).toString()+
+                        "</td></tr>");
+            }
+        }
+        out.println("</tbody>" +
+                            "</table>");
+        if(request.getSession().getAttribute("exceptionMessage")!=null){
+            out.println("<p>"+request.getSession().getAttribute("exceptionMessage")+"</p>");
+            request.getSession().removeAttribute("exceptionMessage");
+        }
+        out.println("<a href=\""+getServletContext().getContextPath()+"/web\">Ссылка на главную страницу</a>");
+        out.println(
+                "</body>" +
+                "</html>");
+        out.close();
     }
 }
